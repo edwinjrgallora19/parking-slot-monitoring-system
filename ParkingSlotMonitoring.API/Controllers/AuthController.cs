111b20450e4482122080a3dade57058fb7d06348
edwinjrgallora19 <edwinjrgallora@gmail.com>
2026-05-10 00:24:36 +0800

    Admin action features has been added

--- ParkingSlotMonitoring.API/Controllers/AuthController.cs
+++ ParkingSlotMonitoring.API/Controllers/AuthController.cs
@@ -33,7 +33,9 @@ namespace ParkingSlotMonitoring.API.Controllers
             {
                 new Claim(ClaimTypes.Name, user.name),
                 new Claim(ClaimTypes.NameIdentifier, user.user_id.ToString()),
-                new Claim("user_id", user.user_id.ToString())
+                new Claim("user_id", user.user_id.ToString()),
+                new Claim(ClaimTypes.Role, user.role_id.ToString())
+
             };
 
             var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
